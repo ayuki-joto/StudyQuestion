73a1481a0e3e69825fd643ae14487d7686f408b8
Adacchi3 <adachi.prog@gmail.com>
2017-10-20 17:18:23 +0900

    develop

---
+++ teacherApp/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import {registerScreens, registerScreenVisibilityListener} from './screens';
+
+registerScreens();
+registerScreenVisibilityListener();
+
+
+const tabs = [{
+  label: 'メンター面談',
+  screen: 'studyquestion.Meeting',
+  icon: require('../img/noimage.png'),
+  title: 'メンター面談',
+}, {
+  label: '先生を選ぶ',
+  screen: 'studyquestion.Calling',
+  icon: require('../img/noimage.png'),
+  title: '電話で質問',
+}, {
+  label: '質問履歴',
+  screen: 'studyquestion.QuestionLog',
+  icon: require('../img/noimage.png'),
+  title: '文章で質問',
+}, {
+  label: 'マイページ',
+  screen: 'studyquestion.Mypage',
+  icon: require('../img/noimage.png'),
+  title: 'マイページ',
+}];
+
+Navigation.startTabBasedApp({
+  tabs,
+  animationType: Platform.OS === 'ios' ? 'fade' : 'fade',
+  tabsStyle: {
+    tabBarBackgroundColor: '#eaeaea',
+    tabBarButtonColor: '#5E6977',
+    tabBarSelectedButtonColor: '#eacd56',
+    tabFontFamily: 'BioRhyme-Bold',
+  },
+  appStyle: {
+    tabBarBackgroundColor: '#003a66',
+    navBarButtonColor: '#ffffff',
+    tabBarButtonColor: '#ffffff',
+    navBarTextColor: '#ffffff',
+    tabBarSelectedButtonColor: '#ff505c',
+    navigationBarColor: '#003a66',
+    navBarBackgroundColor: '#003a66',
+    statusBarColor: '#002b4c',
+    tabFontFamily: 'BioRhyme-Bold',
+  }
+});
---
+++ teacherApp/src/screens/index.js
@@ -0,0 +1,22 @@
+import {Navigation, ScreenVisibilityListener} from 'react-native-navigation';
+
+import Meeting from './MeetingScreen';
+import Mypage from './MypageScreen';
+import QuestionLog from './QuestionLogScreen';
+import Calling from './CallingScreen';
+
+export function registerScreens() {
+  Navigation.registerComponent('studyquestion.Meeting', () => Meeting);
+  Navigation.registerComponent('studyquestion.Mypage', () => Mypage);
+  Navigation.registerComponent('studyquestion.QuestionLog', () => QuestionLog);
+  Navigation.registerComponent('studyquestion.Calling', () => Calling);
+}
+
+export function registerScreenVisibilityListener() {
+  new ScreenVisibilityListener({
+    willAppear: ({screen}) => console.log(`Displaying screen ${screen}`),
+    didAppear: ({screen, startTime, endTime, commandType}) => console.log('screenVisibility', `Screen ${screen} displayed in ${endTime - startTime} millis [${commandType}]`),
+    willDisappear: ({screen}) => console.log(`Screen will disappear ${screen}`),
+    didDisappear: ({screen}) => console.log(`Screen disappeared ${screen}`)
+  }).register();
+}
