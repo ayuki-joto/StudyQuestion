322b600ef9f761386d65dd5479b93e2a0e96ae72
Adacchi3 <adachi.prog@gmail.com>
2017-10-26 19:10:27 +0900

    develop the prototype view of question log

---
+++ teacherApp/src/components/QuestionSearchTextField.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Image, Text } from 'react-native';
+import Icon from 'react-native-vector-icons';
+
+class QuestionSearchTextField extends React.Component<{}> {
+
+    searchQuestion (text) {
+        console.log(text);
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <TextInput 
+                    style={styles.textinput}
+                    placeholder="　🔍 教科や単元、キーワードなど"
+                    placeholderTextColor='#a3a3a3'
+                    onChangeText={(searchText)=>{this.searchQuestion(searchText)}}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#224BA7',
+        flexDirection: 'row', 
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    }, 
+    textinput: {
+        flex: 1,
+        backgroundColor: '#112656', 
+        color: '#a3a3a3',
+        textAlign: 'left',
+        fontSize: 10,
+        paddingTop: 10,
+        paddingRight: 10, 
+        paddingBottom: 10,
+        borderRadius: 10,
+    },
+});
+
+export default QuestionSearchTextField;
--- teacherApp/src/screens/QuestionLogScreen.js
+++ teacherApp/src/screens/QuestionLogScreen.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import QuestionSearchTextField from '../components/QuestionSearchTextField';
+import QuestionList from '../components/QuestionList';
 
 class QuestionLogScreen extends React.Component {
 
@@ -59,37 +54,21 @@ class QuestionLogScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Welcome to QuestionLog!
-      </Text>
-      <Text style={styles.instructions}>
-        To get started, edit App.js
-      </Text>
-      <Text style={styles.instructions}>
-        {instructions}
-      </Text>
-    </View>
+      <View>
+        <QuestionSearchTextField />
+        <View style={styles.container}>
+          <QuestionList />
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#ffffff',
   },
 });
 
