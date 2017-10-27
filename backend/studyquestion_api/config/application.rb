751aa1f933da94cfc9a495c8189a9fd2f3736d89
ayuki_j <ajjt66554@gmail.com>
2017-10-27 13:28:37 +0900

    bug fix

--- backend/studyquestion_api/config/application.rb
+++ backend/studyquestion_api/config/application.rb
@@ -15,17 +15,17 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Credentials' => 'true',
-    'Access-Control-Allow-Origin' => 'http://localhost:3355',
-    'Access-Control-Request-Method' => '*'
-}
 
 module StudyquestionApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Credentials' => 'true',
+        'Access-Control-Allow-Origin' => 'http://localhost:3355',
+        'Access-Control-Request-Method' => '*'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
